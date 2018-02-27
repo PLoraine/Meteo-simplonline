d9e223707d5d486ef81c2b45a289c82958dbe644
PLoraine <loraine.perendevy@gmail.com>
2018-02-27 21:13:38 +0100

    Replace the consolelog by a jquery selector and i call the append function for display the data ajax request.

--- js/app.js
+++ js/app.js
@@ -4,16 +4,14 @@
 $.ajax({
 	url : 'http://api.openweathermap.org/data/2.5/weather?q=Pamiers&lang=fr&units=metric&appid=e488227e348fab4707b37ef061ad3c4a',
 	success : function(data){
-		console.log(data);
 		console.log(data.main);
-		console.log("Humidité est de :" + data.main.humidity);
-		console.log("La température est de :" + data.main.temp);
-		console.log("La pression atmosphérique est de :" + data.main.pressure);
-		console.log("La température min est de : " + data.main.temp_min);
-		console.log("La température max est de : " + data.main.temp_max);
-		console.log(data.coord);
-		console.log("La latitude est de : " + data.coord.lat);
-		console.log("La longitude est de : " + data.coord.lon);
+		$('#cadre').append("Humidité est de :" + data.main.humidity);
+		$('#cadre').append("La températurInsert content, specified by the parameter, to the end of each element in the set of matched elements.e est de :" + data.main.temp);
+		$('#cadre').append("La pression atmosphérique est de :" + data.main.pressure);
+		$('#cadre').append("La température min est de : " + data.main.temp_min);
+		$('#cadre').append("La température max est de : " + data.main.temp_max);
+		$('#cadre').append("La latitude est de : " + data.coord.lat);
+		$('#cadre').append("La longitude est de : " + data.coord.lon);
 	}
 
 });
