4745e2cdfaa82e2b52c34c4172968b31c8e0dfbf
PLoraine <loraine.perendevy@gmail.com>
2018-02-28 15:22:40 +0100

    add display the speed of wind + modification .text in .append.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 $.ajax({
 	url : 'http://api.openweathermap.org/data/2.5/weather?q=Pamiers&lang=fr&units=metric&appid=e488227e348fab4707b37ef061ad3c4a',
 	 success : function(data){
-		console.log(data.main);
+		console.log(data);
 		$('#cadre').append("Le nom de la ville est :" + data.name);
 	 	$('#cadre').append("l'humidité est de " + data.main.humidity);
 		$('#cadre').append("La températurInsert content, specified by the parameter, to the end of each element in the set of matched elements.e est de :" + data.main.temp);
@@ -13,8 +13,9 @@ $.ajax({
 		$('#cadre').append("La température max est de : " + data.main.temp_max);
 		$('#cadre').append("La latitude est de : " + data.coord.lat);
 		$('#cadre').append("La longitude est de : " + data.coord.lon);
+		$('#cadre').append("La vitesse du vent est de :" + data.wind.speed);
 	}
 
 });
 		moment.locale('fr');	
-		$('#date').text(moment().format('LL'));
+		$('#date').append(moment().format('LL'));
