72dcb5dacf5289388f74bb404d40eae66e9fe973
PLoraine <loraine.perendevy@gmail.com>
2018-02-28 11:11:38 +0100

    Add consolelog of city + display the data

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@
 // requête AJAX en jquery 
 $.ajax({
 	url : 'http://api.openweathermap.org/data/2.5/weather?q=Pamiers&lang=fr&units=metric&appid=e488227e348fab4707b37ef061ad3c4a',
-	success : function(data){
+	 success : function(data){
 		console.log(data.main);
-		$('#cadre').append("Humidité est de :" + data.main.humidity);
+		$('#cadre').append("Le nom de la ville est :" + data.name);
+	 	$('#cadre').append("l'humidité est de " + data.main.humidity);
 		$('#cadre').append("La températurInsert content, specified by the parameter, to the end of each element in the set of matched elements.e est de :" + data.main.temp);
 		$('#cadre').append("La pression atmosphérique est de :" + data.main.pressure);
 		$('#cadre').append("La température min est de : " + data.main.temp_min);
