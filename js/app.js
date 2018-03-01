706b25025e8cd342a8e62e95f117958f2586a789
PLoraine <loraine.perendevy@gmail.com>
2018-03-01 15:35:02 +0100

    Add width and height of the card

--- js/app.js
+++ js/app.js
@@ -1,13 +1,4 @@
-//console.log("hello");
 
-// requête AJAX en jquery 
-// $.ajax({
-// 	url : 'http://api.openweathermap.org/data/2.5/weather?q=Pamiers&lang=fr&units=metric&appid=e488227e348fab4707b37ef061ad3c4a',
-// 	 success : function(data){
-// 		//console.log(data);
-// 	}
-
-// });
 		moment.locale('fr');	
 		$('#date').append(moment().format('LL'));
 
@@ -33,7 +24,7 @@ $("#buton").click(function(){
 		});
 
 	}else{
-		$("#error").html('fiels you cannot be empty')
+		$("#error").html('le champ ne peux pas être vide')
 	}
 });
 	
