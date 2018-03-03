308971782a7a661463a1184bb819f7c0280c2c05
PLoraine <loraine.perendevy@gmail.com>
2018-03-03 20:09:12 +0100

    Map finsh

--- js/app.js
+++ js/app.js
@@ -1,37 +1,56 @@
 var lon, lat;
 var city;
+var map;
 moment.locale('fr');
-$('#date').append(moment().format("<p>" + 'LL' + "</p>"));
-
+$('#date').append(moment().format("<p>"+'LL'+"</p>"));
 $("#buton").click(function() {
     var valinput = $("#text").val();
     console.log(valinput);
-    $.ajax({
-        url: 'https://api.openweathermap.org/data/2.5/weather?q=' + valinput + '&units=metric&appid=e488227e348fab4707b37ef061ad3c4a',
-        dataType: "json",
-        success: function(data) {
-            console.log(data);
-            lon = data.coord.lon;
-            lat = data.coord.lat;
-            city = data.name;
-            
-            $('#cadre').html("<li>Temp.max : " + data.main.temp_max + "°</li>")
-                .append("<li>Temp.min: " + data.main.temp_min + "°</li>")
-                .append("<li>Pres.atmosphérique :" + data.main.pressure + "</li>")
-                .append("<li>Vitesse du vent est de :" + data.wind.speed + "km/h</li>")
-                .append("<li>Humidité est de " + data.main.humidity + "%</li>")
-                .append("<li>La latitude est de : " + lat + "</li>")
-                .append("<li>La longitude est de : " + lon + "</li>");
-                $('#map').html("<iframe width='100%' heigth='100%' src='https://www.google.com/maps/embed/v1/view?key=AIzaSyBZ-mRs0nlafD2fmwIMcaUyRtQo3G5GvrU&center=" + lat + "," + lon + "&zoom=11&maptype=satellite'></iframe>");
-                $('#temp').text( + parseInt(data.main.temp)+ "°");
-            
-        }
-    });
+    
+        $.ajax({
+            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + valinput + '&units=metric&appid=e488227e348fab4707b37ef061ad3c4a',
+            dataType:"json", 
+            success: function(data) {
+                console.log(data);
+                lon = data.coord.lon;
+                lat = data.coord.lat;
+                city = data.name;
+                if (map != undefined || map!= null){
+                    map.remove();
+                    map=L.map('map')
+                    carte(map)
+                }else{
+                    map = L.map('map');
+                    carte(map);
+
+                }
+
+                $('#cadre').html( data.main.temp)
+                    $('#temp_max').text( data.main.temp_max + "°")
+                    $('#temp_min').text(data.main.temp_min + "°")
+                    $('#press-at').text(data.main.pressure)
+                    $('#vit-vent').text(data.wind.speed + "km/h")
+                    $('#humidite').text( data.main.humidity + "%")
+                    $('#lon').text(lat)
+                    $('#lat').text(lon)
+                    
+            }
+        });
 });
 
+function carte(map){
 
+    console.log(lon, lat)
+    var ll = [lat, lon]
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 20
+    }).addTo(map.setView(ll, 13));
 
+    //ajout d'un markeur
+    var marker = L.marker(ll).addTo(map);
 
-/// si je j'entre un mot dans l'input et que je clique sur le bouton valider je veu 
+    // ajout d'un popup
+    marker.bindPopup(city);
+}
 // ajout d'un mécanisme pour refraiche la carte suivant la première recherche
 // compatibilité avec les navigateur tous
